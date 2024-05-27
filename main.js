5fc9c52e7bbc9818acb29f7c3dd7ce41c6e8a124
alex M <alexalexdavix@gmail.com>
2024-05-26 20:26:40 -0400

    animacion agregadas, correccion del WhiteMode

--- main.js
+++ main.js
@@ -1,4 +1,59 @@
-document.querySelector('.container').scrollTo({
-    top: 500,
-    behavior: 'smooth'
+btnWhiteMode.addEventListener('click', getWhiteMode);
+bars__menu.addEventListener("click", animateBars);
+btnSectionKnowledge[0].addEventListener("click", () => targetSection[0].scrollIntoView({ behavior: 'smooth' }));
+btnSectionWork[0].addEventListener("click", () => targetSection[1].scrollIntoView({ behavior: 'smooth' }));
+btnSectionMessage[0].addEventListener("click", () => targetSection[2].scrollIntoView({ behavior: 'smooth' }));
+
+function getWhiteMode(){
+    pageOne.classList.toggle('-whiteMode');
+    pageTwo.classList.toggle('-whiteMode');
+    pageThree.classList.toggle('-whiteMode');
+    pageFour.classList.toggle('-whiteMode');
+
+    pageOneMain.classList.toggle('mainMode');
+    pageTwoMain.classList.toggle('mainMode');
+    pageThreeMain.classList.toggle('mainMode');
+
+    bars__menu.classList.toggle('mode');
+    
+    lettersP.forEach(p => {
+      p.classList.toggle('letters');
   });
+    lettersH1.forEach(h1 => {
+      h1.classList.toggle('letters');
+  });
+    lettersH2.forEach(h2 => {
+      h2.classList.toggle('letters');
+  });
+    lettersH3.forEach(h3 => {
+      h3.classList.toggle('letters');
+  });
+    lettersH4.forEach(h4 => {
+      h4.classList.toggle('letters');
+  });
+
+  borderBar.forEach(c => {
+    c.classList.toggle('borderBar');
+});
+  iconsTitle.forEach(d => {
+    d.classList.toggle('iconMode');
+});
+
+}
+
+function animateBars(){
+    line1__bars.classList.toggle("activeline1__bars-menu");
+    line2__bars.classList.toggle("activeline2__bars-menu");
+    line3__bars.classList.toggle("activeline3__bars-menu");
+
+    paragraph.forEach(c => {
+      c.classList.toggle('hidden');
+  });
+  menuShift.forEach(m => {
+      m.classList.toggle('show');
+
+      setTimeout(() => {
+        m.classList.toggle('animate');
+    }, 800);
+  });
+}
---
+++ nodes.js
@@ -0,0 +1,36 @@
+//darMode///////
+const btnWhiteMode = document.querySelector('.whiteMode');
+
+const pageOne = document.querySelector('.one');
+const pageTwo = document.querySelector('.two');
+const pageThree = document.querySelector('.three');
+const pageFour = document.querySelector('.four');
+
+const pageOneMain = document.querySelector('.main');
+const pageTwoMain = document.querySelector('.main-two');
+const pageThreeMain = document.querySelector('.main-message');
+const pagefourMain = document.querySelector('.main-three');
+
+const lettersP = document.querySelectorAll('p');
+const lettersH1 = document.querySelectorAll('h1');
+const lettersH2 = document.querySelectorAll('h2');
+const lettersH3 = document.querySelectorAll('h3');
+const lettersH4 = document.querySelectorAll('h4');
+
+const borderBar = document.querySelectorAll('.rect-1');
+
+const iconsTitle = document.querySelectorAll('.color');
+
+/// menu ////////
+const bars__menu = document.querySelector(".bars__menu");
+const line1__bars = document.querySelector(".line1__bars-menu");
+const line2__bars = document.querySelector(".line2__bars-menu");
+const line3__bars = document.querySelector(".line3__bars-menu");
+
+const paragraph = document.querySelectorAll(".paragraph");
+const menuShift = document.querySelectorAll(".menuShift--content"); 
+
+const btnSectionKnowledge = document.querySelectorAll('.knowledge--content')
+const btnSectionWork = document.querySelectorAll('.work--content')
+const btnSectionMessage = document.querySelectorAll('.send--content')
+const targetSection = document.querySelectorAll('.page');
